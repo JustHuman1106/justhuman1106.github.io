9f6462f8088c7b7082e597328f39116e9aac67d6
my <gminho906@gmail.com>
2022-05-22 00:42:36 +0900

    dasd

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function MyApp({ Component, pageProps }) {
+    return <Component ponent {...pageProps} />;
+}
+  
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -4,61 +4,22 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Introduction site</title>
+        <link rel="icon" href="/logo.png" />
       </Head>
-
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          안녕하세요
         </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h2 className="title-1">
+          이름은 던첸입니다.
+        </h2>
       </main>
-
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+        <a target="_blank" rel="noopener noreferrer">
+          © 2022 dunchen All Rights Reserved.
         </a>
       </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -116,7 +77,7 @@ export default function Home() {
         .title {
           margin: 0;
           line-height: 1.15;
-          font-size: 4rem;
+          font-size: 6rem;
         }
 
         .title,
@@ -206,4 +167,5 @@ export default function Home() {
       `}</style>
     </div>
   )
+  
 }
